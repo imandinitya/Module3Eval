225785910dca1d1166a19d16315ef902d849c210
Nitya Imandi <nitya3186@gmail.com>
2026-01-03 21:03:01 +0530

    Refactor AdminDashboard and integrate RestaurantCard

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,32 +1,16 @@
-import { useNavigate } from "react-router-dom";
-import {useAuth} from "../context/AuthContext";
+import AddRestaurantForm  from "../Components/AddRestaurantForm";
+import RestaurantCard from "../Components/RestaurantCard";
 import { useRestaurants } from "../context/RestaurantContext";
 
-export default function RestaurantCard({restaurant}) {
-    {
-        const {auth} = useAuth();
-        const {deleteRestaurant} = useRestaurants();
-        const navigate = useNavigate();
-        const handleDelete = () => {
-            if (window.confirm("Are you sure you want to delete this restaurant?")) {
-                deleteRestaurant(data.restaurant.id);
-            }
-        }
-    };
-    return(
-        <div style={{border: "1px solid black", margin: 10}}>
-            <h3>{data.restaurant.name}</h3>
-            <p>{data.address}</p>
-            <p>{data.type}</p>
-            <p>Parking: {data.parkingLot ? "yes" : "no"}</p>
-            {auth.role === "admin" && (
-                <>
-                    <button onClick={() => navigate(`/admin/edit-restaurant/${data.restaurant.ID}`)}>Edit</button>
-                    <button onClick={handleDelete}>Delete</button>
-                </>
-            )}
-
+export default function AdminDashboard() {
+    const { restaurants } = useRestaurants();
+    return (
+        <div>
+            <h2>admin dashboard</h2>
+            <AddRestaurantForm/>
+            {restaurants.map((r) => (
+                <RestaurantCard key={r.id} restaurant={r} />
+            ))}
         </div>
     );
-
 }
